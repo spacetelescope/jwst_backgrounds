417d9fd7ca73e532c4afbe5465025ed602de2c71
Klaus Pontoppidan <pontoppi@stsci.edu>
2017-06-17 11:25:55 -0400

    update to python 3 compatibility

--- jwst_backgrounds.py
+++ jwst_backgrounds.py
@@ -1,4 +1,5 @@
-''' This is a simple program to predict the background levels for JWST observations, 
+''' 
+This is a simple program to predict the background levels for JWST observations, 
 for use in proposal planning.
 It accesses a precompiled background cache prepared by STScI, to do the following:
 - Plot the background versus calendar day.
@@ -7,13 +8,11 @@ It accesses a precompiled background cache prepared by STScI, to do the followin
 This code was written by Jane Rigby, Jane.Rigby@nasa.gov
 The background cache was prepared by Wane Kinzel at STScI.  
 Software is provided as-is, with no warranty.
-
  Here is the schema for the precompiled background cache.
  (JRR verified the schema against the source code, generate_stray_light_with_threads.c)
  The cache uses a Healpix RING tesselation, with NSIDE=128.  Every point on the sky 
  (tesselated tile) corresponds to one binary file, whose name includes its healpix 
  pixel number, in a directory corresponding to the first 4 digits of the healpix number.  
-
 Here's the schema of each binary file in the cache:
 double RA
 double DEC
@@ -23,13 +22,10 @@ int[366] date_map  # This maps dates to indices.  **There are 366 days, not 365!
 for each day in FOR:
   double zodi_bg[SL_NWAVE]
   double stray_light_bg[SL_NWAVE]
-
-
 Note: This is a subset of Rigby's longer code JRR_Code/read_JWST_bkg.py .  This is
 just the part that a user needs to make a background-versus-calendar "bathtub" curve.
 '''
 
-#import jrr  # Moved these routines to this file, for portability.
 import glob
 import subprocess
 from os.path import basename
@@ -40,7 +36,6 @@ import numpy as np
 import pandas
 from scipy.interpolate import interp1d
 from matplotlib import pyplot as plt
-#import seaborn as sns
 from matplotlib.backends.backend_pdf import PdfPages
 
 #### User should change these paths  ############################################
@@ -103,7 +98,7 @@ def read_JWST_precompiled_bkg(infile, bkg_dir, showplot=False, thisday=-99, verb
     sbet_file = open(bkg_dir + infile)
     sbet_data = sbet_file.read()
     # Unpack the constant first part
-    if verbose: print "File has", len(sbet_data), "bytes, which is", len(sbet_data)/8., "doubles"
+    if verbose: print("File has", len(sbet_data), "bytes, which is", len(sbet_data)/8., "doubles")
     size_calendar = struct.calcsize("366i") # bytes, not doubles
     partA = struct.unpack(str(5 + SL_NWAVE)+'d', sbet_data[0: (5 + SL_NWAVE)*8])
     RA = partA[0]
@@ -113,13 +108,11 @@ def read_JWST_precompiled_bkg(infile, bkg_dir, showplot=False, thisday=-99, verb
 
     # Unpack the calendar dates      # code goes from 0 to 365 days.
     date_map = np.array(struct.unpack('366i', sbet_data[(5 + SL_NWAVE)*8  : (5 + SL_NWAVE)*8 + size_calendar]))
-    if verbose: print "Out of", len(date_map), "days, these many are legal:", np.sum(date_map >=0)
-    #print "indices of days:", date_map[date_map>=0]
+    if verbose: print("Out of", len(date_map), "days, these many are legal:", np.sum(date_map >=0))
     calendar = np.where(date_map >=0)[0]
-    #print "calendar date:", calendar
     # So, the index dd in zodi_bg[dd, : ]  corresponds to the calendar day lookup[dd]
     Ndays = len(calendar) 
-    if verbose: print len(date_map), Ndays
+    if verbose: print(len(date_map), Ndays)
 
     # Unpack part B, the time-variable part
     zodi_bg        = np.zeros((Ndays,SL_NWAVE))
@@ -131,7 +124,6 @@ def read_JWST_precompiled_bkg(infile, bkg_dir, showplot=False, thisday=-99, verb
         br1 = dd*perday
         br2 = br1 + SL_NWAVE
         br3 = br2 + SL_NWAVE
-        #print "Breaking at:", br1, br2, br3
         zodi_bg[dd, ]        = partB[br1 : br2]
         stray_light_bg[dd, ] = partB[br2 : br3]
     expand = np.ones((Ndays,SL_NWAVE))  # same shape as zodi_bg
@@ -142,7 +134,7 @@ def read_JWST_precompiled_bkg(infile, bkg_dir, showplot=False, thisday=-99, verb
         plt.clf()
         if thisday in calendar : pass
         else :   thisday = find_nearest(calendar, np.mean(calendar))  # plot the middle of the calendar
-        print "Plotting spectrum for calendar day", thisday
+        print("Plotting spectrum for calendar day", thisday)
         plt.plot(wave_array, nonzodi_bg, label="ISM")
         plt.plot(wave_array, zodi_bg[thisday, :], label="Zodi")
         plt.plot(wave_array, stray_light_bg[thisday, :], label="Stray light")
@@ -183,7 +175,6 @@ def make_bathtub(results, wavelength_desired, thresh, showthresh=True, showplot=
     if showplot:
         if showannotate:
             annotation = str(allgood) + " good days out of " + str(len(calendar)) + " days observable, for thresh " + str(thresh)
-            #print str(wavelength_desired) + " " + annotation
             plt.annotate(annotation, (0.05,0.05), xycoords="axes fraction", fontsize=12)
         if not label : label="Total " + str(wavelength_desired) + " micron"
         plt.scatter(calendar, total_thiswave, s=20, label=label)
@@ -220,30 +211,30 @@ def plot_bkg(RA, DEC, wavelength_input, thresh=1.1, plot_spec=True, thisday=-99,
     #         print2file   [bool], whether to print the background levels that are plotted in plot_days to an outfile
     #         outfile:     output filename
     if not (RA and DEC and wavelength_input) : raise Exception("Required arguments for plot_bkg are RA, DEC, and wavelength.  Optional argument is threshold.")
-    print "Plotting the background versus calendar day for RA, DEC, wave, thresh of:", RA, DEC, wavelength_input, thresh
+    print("Plotting the background versus calendar day for RA, DEC, wave, thresh of:", RA, DEC, wavelength_input, thresh)
     healpix = healpy.pixelfunc.ang2pix(nside, RA, DEC, nest=False, lonlat=True)  # old versions of healpy don't have lonlat
     myfile = myfile_from_healpix(healpix)   # Retrieve the name of the healpix file, including leading zero formatting
 
-    if plot_spec:  print "Plotting the background spectrum for an example day.  Turn this off w plot_spec=False"
+    if plot_spec:  print("Plotting the background spectrum for an example day.  Turn this off w plot_spec=False")
     results = read_JWST_precompiled_bkg(myfile, bkg_dir, showplot=plot_spec, thisday=thisday)  # Retrieve the bkg file
     (calendar, RA, DEC, pos, wave_array, nonzodi_bg, thermal, zodi_bg, stray_light_bg, total)  = results  # parse results
     wavelength_desired = find_nearest(wave_array, wavelength_input)  # Nearest neighbor interpolation of wavelength
     if wavelength_desired != wavelength_input :
-        print "Using wave", wavelength_desired, "as the nearest neighbor to input", wavelength_input, "micron"
+        print("Using wave", wavelength_desired, "as the nearest neighbor to input", wavelength_input, "micron")
     allgood = make_bathtub(results, wavelength_desired, thresh, showplot=plot_days, showsubbkgs=showsubbkgs)  # Compute bathtub, plot it.
-    if plot_days : print "Plotting background versus calendar day of the year"
+    if plot_days : print("Plotting background versus calendar day of the year")
     plt.show()
-    print "RESULTS:  These coordinates are observable by JWST", len(calendar), "days per year."
-    print "RESULTS:  For", allgood, "of those days, the background is <", thresh, "of the minimum, at wavelength", wavelength_desired, "micron"
+    print("RESULTS:  These coordinates are observable by JWST", len(calendar), "days per year.")
+    print("RESULTS:  For", allgood, "of those days, the background is <", thresh, "of the minimum, at wavelength", wavelength_desired, "micron")
     if print2file:  # print the total background levels
          the_index = index_of_wavelength(wave_array, wavelength_desired)
          bath = pandas.DataFrame({'calendar_day': calendar, 'total_bkg' : total[ :, the_index]}).set_index('calendar_day')
          bath.to_csv("tempfile")
-         print "Just wrote total background versus calendar day to", outfile, "\nHere is a subset:"
+         print("Just wrote total background versus calendar day to", outfile, "\nHere is a subset:")
          header_text = "#Output of JWST_backgrounds.py, plot_bkg().\n# Columns are Calendar day (Jan1=0) and total background (in MegaJanskies per sterradian)\n"
          header_text += "#  for  RA="+str(RA)+ "   DEC="+str(DEC)+"    wave="+str(wavelength_desired)+" micron\n"
          put_header_on_file("tempfile", header_text, outfile) 
-         print bath.head(10)
+         print(bath.head(10))
     return(results)
 
 ## Here is an example of using this code:
