279eb3df2601e0fc8ead58f92f0ef6df03212bfa
Klaus Pontoppidan <pontoppi@stsci.edu>
2017-10-23 15:57:47 -0400

    Added pontoppidan to author name, added a download URL for pypi

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(name='jwst_backgrounds',
       version='1.0',
       description='Retrieve and plot JWST background information',
-      author='Jane Rigby',
+      author='Jane Rigby (GSFC) and Klaus Pontoppidan (STScI)',
       author_email='Jane.Rigby@nasa.gov',
       url='http://jwst.stsci.edu/',
+      download_url = 'https://github.com/spacetelescope/jwst_backgrounds/archive/jwst_backgrounds_1.0.tar.gz',
       packages=['jwst_backgrounds'],
       package_data={'jwst_backgrounds': ['refdata/*.csv','refdata/*.txt']},
       install_requires=['healpy>=1.10']
