c9c16e77e746fe0a63b19ece2cdb48bf3b9801c7
Mees Fix <mfix@stsci.edu>
2019-12-06 15:44:40 -0500

    Updataing setup and adding requirements. All third party software should be included now

--- setup.py
+++ setup.py
@@ -11,7 +11,11 @@ setup(name='jwst_backgrounds',
       download_url = 'https://github.com/spacetelescope/jwst_backgrounds/',
       packages=['jwst_backgrounds'],
       package_data={'jwst_backgrounds': ['refdata/*.csv','refdata/*.txt','agreement/*.pdf']},
-      install_requires=['healpy>=1.10'],
+      install_requires=['healpy>=1.10',
+                        'healpy>=1.10',
+                        'matplotlib==3.1.1',
+                        'numpy==1.17.0',
+                        'scipy==1.1.0'],
       entry_points = {'console_scripts': ['jwst_backgrounds=jwst_backgrounds.cli:main']}
       )
 
