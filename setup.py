e063a28f1cb0ccd0bc4151ce15134cbe51b1e7f3
Mees Fix <mfix@stsci.edu>
2022-11-28 15:11:26 -0500

    Adding long description and updates to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
+from os import path
 from setuptools import setup
 
 from jwst_backgrounds.version import __version__
 
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='jwst_backgrounds',
       version=__version__,
+      description='JWST Backgrounds',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+
+      # The project's main homepage.
+      url='https://github.com/spacetelescope/jwst_background',
+
+      # Author details
+      author='Space Telescope Science Institute',
+
+      # Choose your license
+      license='BSD',
       description='Retrieve and plot JWST background information',
       author='Jane Rigby (GSFC) and Klaus Pontoppidan (STScI)',
       author_email='Jane.Rigby@nasa.gov',
@@ -17,5 +36,3 @@ setup(name='jwst_backgrounds',
                         'scipy>=1.1.0'],
       entry_points = {'console_scripts': ['jwst_backgrounds=jwst_backgrounds.cli:main']}
       )
-
-    
