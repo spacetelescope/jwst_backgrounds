ede8721d8ccf17996ebb6cb90a10047703cc4114
Mees Fix <mfix@stsci.edu>
2024-07-15 14:29:19 -0700

    Removed extra quotes

--- jwst_backgrounds/tests/test_jwst_bkg.py
+++ jwst_backgrounds/tests/test_jwst_bkg.py
@@ -9,7 +9,7 @@ from jwst_backgrounds.jbt import background
 
 TEST_DIR = abspath(join(dirname(__file__)))
 
-@pytest.mark.parametrize("ra, dec, expected", [(82.82, -5.39, "'1073/sl_pix_107381.bin'"), (10.68471, 41.26875, "'0330/sl_pix_033039.bin'")])
+@pytest.mark.parametrize("ra, dec, expected", [(82.82, -5.39, "1073/sl_pix_107381.bin"), (10.68471, 41.26875, "0330/sl_pix_033039.bin")])
 def test_myfile_from_healpix(ra, dec, expected):
     bkg = background(ra, dec, 2.15)
     healpix_file = bkg.myfile_from_healpix(ra, dec)
